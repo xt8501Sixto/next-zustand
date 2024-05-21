3c216c40016dcdc9271dea26f277d3d6cf89c1bc
Sixto Mujica <sjmujica@gmail.com>
2024-05-21 08:59:21 -0500

    add redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,6 @@
 import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  if (typeof window !== "undefined") {
-    const user = localStorage.clear()
-    return user;
-  }
   redirect("/auth/login");
 
 }
