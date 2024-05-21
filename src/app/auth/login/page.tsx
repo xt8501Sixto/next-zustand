869cf747d3e54e38af55dd2ac7fbe147c70d8524
xt8512 <xt8512@rimac.com.pe>
2024-05-21 13:16:25 -0500

    feat:login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -7,7 +7,7 @@ export default function LoginPage() {
       <div className="relative flex items-center w-3/5">
         <div className="w-full z-10">
           <div className="text-wrap">
-            <h2 className="ride-sys-color-border-feedback-critical text-5xl font-bold ride-sys-text-title-8xlarge-default ride-sys-color-background-highlight-default">
+            <h2 className="font-bold ride-sys-text-title-8xlarge-default text-ride-sys-color-text-highlight">
              ¡Hola!
             </h2>
             <p className="mt-2 text-xl text-gray-600">
